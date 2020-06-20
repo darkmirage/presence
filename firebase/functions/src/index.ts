b4a50c0f4a5e1c2902fb7f80751e33c2a794f158
Raven Jiang <raven@cs.stanford.edu>
2020-06-20 00:12:27 -0700

    CORS for cloud function

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,30 +1,34 @@
 import * as functions from 'firebase-functions';
 import { jwt } from 'twilio';
+import * as cors from 'cors';
 
+const corsHandler = cors({ origin: true });
 const { AccessToken } = jwt;
 const ACCOUNT_SID = functions.config().twilio.asid;
 const API_KEY_SID = functions.config().twilio.sid;
 const API_KEY_SECRET = functions.config().twilio.secret;
 
 export const getTwilioToken = functions.https.onRequest((request, response) => {
-  const { username, room } = request.query;
+  return corsHandler(request, response, () => {
+    const { username, room } = request.query;
 
-  if (typeof username !== 'string' || typeof room !== 'string') {
-    response.status(500).send({ error: 'Invalid username or room' });
-    return;
-  }
+    if (typeof username !== 'string' || typeof room !== 'string') {
+      response.status(500).send({ error: 'Invalid username or room' });
+      return;
+    }
 
-  const accessToken = new AccessToken(
-    ACCOUNT_SID,
-    API_KEY_SID,
-    API_KEY_SECRET,
-    { identity: username }
-  );
+    const accessToken = new AccessToken(
+      ACCOUNT_SID,
+      API_KEY_SID,
+      API_KEY_SECRET,
+      { identity: username }
+    );
 
-  const grant = new AccessToken.VideoGrant({ room });
-  accessToken.addGrant(grant);
-  const token = accessToken.toJwt();
+    const grant = new AccessToken.VideoGrant({ room });
+    accessToken.addGrant(grant);
+    const token = accessToken.toJwt();
 
-  console.log(username, room, token);
-  response.send({ token });
+    console.log(username, room, token);
+    response.send({ token });
+  });
 });

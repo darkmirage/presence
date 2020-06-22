67ecf61b790389f8f8965399f9649d4ce94ddb36
Raven Jiang <raven@cs.stanford.edu>
2020-06-21 20:21:24 -0700

    KalmanFilter

--- web/src/cv/PoseEstimator.ts
+++ web/src/cv/PoseEstimator.ts
@@ -2,12 +2,14 @@ import { Pose, Keypoint } from '@tensorflow-models/posenet';
 import { Vector3, PerspectiveCamera, Matrix4 } from 'three';
 
 import solveP3P from './solveP3P';
+import KalmanFilter from './KalmanFilter';
 
 const MEAN_IPD = 0.063;
 
 const _left = new Vector3();
 const _right = new Vector3();
 const _nose = new Vector3();
+const _v = new Vector3();
 
 const _leftWorld = new Vector3(-MEAN_IPD / 2, 0.03175, -0.0254);
 const _rightWorld = new Vector3(MEAN_IPD / 2, 0.03175, -0.0254);
@@ -19,18 +21,24 @@ class PoseEstimator {
   camera: PerspectiveCamera;
   position: Vector3;
   orientation: Matrix4;
+  _filters: Record<string, KalmanFilter>;
 
   constructor() {
     this.imageHeight = 1024;
     this.imageWidth = 768;
     this.camera = new PerspectiveCamera(
-      50,
+      78,
       this.imageWidth / this.imageHeight,
       0.1,
       100
     );
     this.position = new Vector3();
     this.orientation = new Matrix4();
+    this._filters = {
+      x: new KalmanFilter(),
+      y: new KalmanFilter(),
+      z: new KalmanFilter(),
+    };
   }
 
   _normalizePixels(p: Vector3): Vector3 {
@@ -74,8 +82,27 @@ class PoseEstimator {
       _noseWorld
     );
 
-    this.position.copy(solutions[1].c);
-    const R = solutions[1].R;
+    let minDistance = Infinity;
+    let I = -1;
+    for (let i = 0; i < solutions.length; i += 1) {
+      const { c } = solutions[i];
+      const d = _v.subVectors(c, this.position).length();
+      if (d < minDistance) {
+        I = i;
+        minDistance = d;
+      }
+    }
+
+    if (I === -1) {
+      return;
+    }
+
+    const { c, R } = solutions[I];
+    const x = this._filters.x.filter(c.x);
+    const y = this._filters.y.filter(c.y);
+    const z = this._filters.z.filter(c.z);
+
+    this.position.set(x, y, z);
     const e = R.elements;
     this.orientation.set(
       e[0],
